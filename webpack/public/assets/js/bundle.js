947473c0d69507eed9c7e402e1b1a127131dc925
Davi Souza <davissbf@gmail.com>
2020-11-06 15:34:31 -0300

    origin

---
+++ webpack/public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+alert("Olá Mundo!");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
