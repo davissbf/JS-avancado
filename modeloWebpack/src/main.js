cdea27479628d740561ff7bea749a21a24c8eff5
Davi Souza <davissbf@gmail.com>
2020-11-12 13:27:59 -0300

    CPFGerado

--- modeloWebpack/src/main.js
+++ modeloWebpack/src/main.js
@@ -1,5 +1,9 @@
-import GeraCPF from './module/GeraCPF';
+import GeraCPF from './modules/GeraCPF';
 
 import './assets/css/style.css';
 
-
+(function() {
+  const gera = new GeraCPF();
+  const cpfGerado = document.querySelector('.cpf-gerado');
+  cpfGerado.innerHTML = gera.geraNovoCpf();
+})();
--- modeloWebpack/src/modules/ValidaCPF.js
+++ modeloWebpack/src/modules/ValidaCPF.js
@@ -43,12 +43,3 @@ export default class ValidaCPF {
     return this.novoCPF === this.cpfLimpo;
   }
 }
-
-let validacpf = new ValidaCPF('070.987.720-03');
-// validacpf = new ValidaCPF('999.999.999-99');
-
-if (validacpf.valida()) {
-  console.log('CPF válido');
-} else {
-  console.log('CPF inválido');
-}
