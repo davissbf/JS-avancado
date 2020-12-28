e157c5079dcc204519ce6a8456f30850283e943e
Davi Souza <davissbf@gmail.com>
2020-12-28 15:51:25 -0300

    router

--- node/aula09/router.js
+++ node/aula09/router.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('./src/controller/homeController');
-const homeContato = require('./src/conttoller/contatoController');
+const homeController = require('./controller/homeController');
 
 router.get('/', homeController.paginaInicial);
 router.post('/', homeController.trataPost);
-
-router.get('/contato', homeContato.paginaInicial);
 
 module.exports = router;
--- node/aula10/router.js
+++ node/aula10/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('./controller/Homecontroller');
+const homeController = require('./src/controller/homeController');
+const homeContato = require('./src/conttoller/contatoController');
 
 router.get('/', homeController.paginaInicial);
 router.post('/', homeController.trataPost);
+
+router.get('/contato', homeContato.paginaInicial);
 
 module.exports = router;
