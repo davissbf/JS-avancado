5b26f4cd59bfc367d4a1e2ffa78f05bfc580fa5b
Davi Souza <davissbf@gmail.com>
2021-01-22 17:33:40 -0300

    criando templates e rotas

--- agenda/router.js
+++ agenda/router.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('./src/controller/homeController');
-const homeContato = require('./src/controller/contatoController');
+const loginController = require('./src/controller/loginController');
 
-router.get('/', homeController.paginaInicial);
-router.post('/', homeController.trataPost);
-
-router.get('/contato', homeContato.paginaInicial);
+router.get('/', homeController.index);
+router.get('/login', loginController.index);
 
 module.exports = router;
